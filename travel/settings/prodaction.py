c7934cb8afe71f24a3f799680987d26a5a91ca99
DVorobiev1968 <vdv@rdturbo.ru>
2021-11-03 19:54:00 +0500

    Указал в prodaction.py наименование рессурса с проектом на Heroku: ALLOWED_HOSTS = ['travel-find-route-dvorobiev.herokuapp.com']

--- travel/settings/prodaction.py
+++ travel/settings/prodaction.py
@@ -30,7 +30,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['find-route.herokuapp.com']
+ALLOWED_HOSTS = ['travel-find-route-dvorobiev.herokuapp.com']
 
 
 # Application definition
